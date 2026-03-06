e17ac4684e88c968b03b6e18b6a301c9696ba8c1
Andrew <chiron1600@gmail.com>
2026-03-06 20:51:50 +0200

    remove import zod

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const Product = require("./models/Product");
-const { json } = require("zod");
 
 dotenv.config();
 
